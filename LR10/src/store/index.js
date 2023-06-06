87abc8343061c152baa2e6a946a0ffedea299107
Kliment Sheremetev <101826150+Rudylier@users.noreply.github.com>
2023-06-06 23:40:36 +0300

    Add files via upload

---
+++ LR10/src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from 'vuex'
+import paintings from './paintings';
+import halls from './halls';
+export default createStore({
+  modules: {
+    paintings,
+    halls,
+  },
+  state: {},
+  mutations: {},
+  actions: {},
+})
