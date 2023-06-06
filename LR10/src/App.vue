8b8c7d089cbd05e75655192fd748aab4e0b42258
Kliment Sheremetev <101826150+Rudylier@users.noreply.github.com>
2023-06-06 23:37:49 +0300

    Add files via upload

---
+++ LR10/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<Header />
+  
+  <router-view v-slot="{ Component }">
+    <keep-alive>
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
+</template>
+
+<script setup>
+import Header from './components/Header.vue';
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+}
+</style>
